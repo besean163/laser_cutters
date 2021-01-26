7942e4a3eb450f70c99810b4ff5c95a029cda0d4
besean163 <besean163@yandex.ru>
2021-01-26 22:42:52 +0400

    add from home PC

---
+++ LaserCutters/add.php
@@ -0,0 +1,14 @@
+<?php
+
+    
+    include_once "core/productProperties.php";
+    include_once "patterns/p_add.php";
+    include_once "../Helper/Helper.php";
+
+    Helper::myDump($_POST);
+
+
+
+
+
+?>
